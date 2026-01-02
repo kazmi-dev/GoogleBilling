a9094057f7e981da496f26d9f809ffc6bda0a32d
Syed Turaab Haider Kazmi <syedzainkazmi78@gmail.com>
2026-01-02 19:18:47 +0500

    Update BillingEvent.kt

--- billing/billing_with_di/BillingEvent.kt
+++ billing/billing_with_di/BillingEvent.kt
@@ -1,8 +1,7 @@
-package com.kazmi.dev.coding.base.billing
-
 sealed class BillingEvent {
     data class PurchaseSuccess(val productId: String) : BillingEvent()
     data class PurchaseRestore(val productId: String) : BillingEvent()
     data class PurchasePending(val productId: String) : BillingEvent()
     data class Error(val message: String) : BillingEvent()
+
 }
